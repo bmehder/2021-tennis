446e48ae8152739d63bcd219e10f1c73d20f5e36
bmehder <bmehder@gmail.com>
2021-02-27 21:00:23 -0600

    tiebreak func working

--- src/App.svelte
+++ src/App.svelte
@@ -2,12 +2,6 @@
   const l = (value) => console.log(value);
   $: l(isTiebreak);
 
-  // The sets will need to be an array
-  // in order to increment to the next
-  // array index when a player wins a set.
-
-  // Keep track of the current set with a var.
-  //
   $: score = {
     p1: {
       name: "Roger",
@@ -23,71 +17,55 @@
     },
   };
 
-  // $: score = {
-  //   p1: {
-  //     s1: 5,
-  //     s2: 0,
-  //     s3: 0,
-  //     pt: 0,
-  //     tb: 0,
-  //   },
-  //   p2: {
-  //     s1: 5,
-  //     s2: 0,
-  //     s3: 0,
-  //     pt: 0,
-  //     tb: 0,
-  //   },
-  // };
-
   let isDeuce;
   let isTiebreak;
-
-  let setIndex = 0;
+  let setNum = 0;
 
   $: score.p1.pt === 40 && score.p2.pt === 40
     ? (isDeuce = true)
     : (isDeuce = false);
 
-  $: score.p1.sets[setIndex] === 6 && score.p2.sets[setIndex] === 6
+  $: score.p1.sets[setNum] === 6 && score.p2.sets[setNum] === 6
     ? (isTiebreak = true)
     : (isTiebreak = false);
 
-  const scoreGame = (player) => {
-    if (!isTiebreak) {
-      if (score.p1.pt === "Ad" || score.p2.pt === "Ad") {
-        if (player.pt === "Ad") {
-          player.sets[setIndex] = +player.sets[setIndex] + 1;
-          resetPoints();
-          score = score;
-        } else {
-          score.p1.pt = 40;
-          score.p2.pt = 40;
-        }
-        return;
-      }
+  const handleGame = (player) => {
+    if (isTiebreak) {
+      scoreTiebreak(player);
+    } else if (score.p1.pt === "Ad" || score.p2.pt === "Ad") {
+      scoreAdPoint(player);
+      return;
+    } else if (!isDeuce) {
+      score.p1.pt <= 40 && score.p2.pt <= 40 && scoreNormalPoint(player);
+    } else {
+      // it's deuce
+      player.pt = "Ad";
+      isDeuce = false;
+    }
+    score = score;
+  };
 
-      if (!isDeuce) {
-        if (score.p1.pt <= 40 && score.p2.pt <= 40) {
-          if (player.pt === 0) {
-            player.pt = 15;
-          } else if (player.pt === 15) {
-            player.pt = 30;
-          } else if (player.pt === 30) {
-            player.pt = 40;
-          } else {
-            player.sets[setIndex] = +player.sets[setIndex] + 1;
-            resetPoints();
-          }
-        }
-      } else {
-        // it's deuce
-        player.pt = "Ad";
-        isDeuce = false;
-      }
+  const scoreNormalPoint = (player) => {
+    if (player.pt === 0) {
+      player.pt = 15;
+    } else if (player.pt === 15) {
+      player.pt = 30;
+    } else if (player.pt === 30) {
+      player.pt = 40;
+    } else {
+      player.sets[setNum] = +player.sets[setNum] + 1;
+      resetPoints();
+    }
+  };
+
+  const scoreAdPoint = (player) => {
+    if (player.pt === "Ad") {
+      player.sets[setNum] = +player.sets[setNum] + 1;
+      resetPoints();
       score = score;
     } else {
-      scoreTiebreak(player);
+      score.p1.pt = 40;
+      score.p2.pt = 40;
     }
   };
 
@@ -97,16 +75,16 @@
   };
 
   const scoreTiebreak = (player) => {
-    if (isTiebreak) {
-      player.tb += 1;
-      if (score.p1.tb >= 7 && score.p2.tb < score.p1.tb) {
-        player.s1 += 1;
-        isTiebreak = false;
-      } else if (score.p1.tb > 6 || score.p2.tb > 6) {
-        player.s1 += 1;
-        isTiebreak = false;
-      }
-      score = score;
+    player.tb += 1;
+    if (
+      (score.p1.tb >= 7 && score.p2.tb + 1 < score.p1.tb) ||
+      (score.p2.tb >= 7 && score.p1.tb + 1 < score.p2.tb)
+    ) {
+      console.log("Player 1 wins!");
+      player.sets[setNum] = +player.sets[setNum] + 1;
+      setNum += 1;
+      isTiebreak = false;
+      resetPoints();
     }
   };
 </script>
@@ -118,12 +96,14 @@
     <span>2</span>
     <span>3</span>
     <span>Point</span>
-    <input value="Player 1" />
+
+    <input bind:value={score.p1.name} />
     <input bind:value={score.p1.sets[0]} />
     <input bind:value={score.p1.sets[1]} />
     <input bind:value={score.p1.sets[2]} />
     <input bind:value={score.p1.pt} />
-    <input value="Player 2" />
+
+    <input bind:value={score.p2.name} />
     <input bind:value={score.p2.sets[0]} />
     <input bind:value={score.p2.sets[1]} />
     <input bind:value={score.p1.sets[2]} />
@@ -131,8 +111,8 @@
   </article>
 
   <div>
-    <button on:click={() => scoreGame(score.p1)}>Player 1</button>
-    <button on:click={() => scoreGame(score.p2)}>Player 2</button>
+    <button on:click={() => handleGame(score.p1)}>Player 1</button>
+    <button on:click={() => handleGame(score.p2)}>Player 2</button>
   </div>
 
   {#if score.p1.tb > 0 || score.p2.tb > 0}
